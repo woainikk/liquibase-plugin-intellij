67a42ddc2b0cccbaed8a53b60b8f0a61308bc11f
woainikk <woainimrgrt@gmail.com>
2019-01-03 12:06:09 +0300

    add button generate changeset in group liquibase which doing nothing yet

---
+++ src/main/kotlin/GenerateChangeSet.kt
@@ -0,0 +1,10 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GenerateChangeSet : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent?) {
+
+    }
+
+}
